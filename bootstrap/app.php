00293beb059c80ced52febd14b76fed7d33adaa3
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-08-16 00:20:33 +0600

    update db

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,27 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+$app = new Illuminate\Foundation\Application(
+    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+);
+
+$environmentFile = '.env'; // Default environment file
+
+$domain = $_SERVER['HTTP_HOST'] ?? '';
+
+if ($domain === 'subscription.soft-itbd.com') {
+    $environmentFile = '.env.subscription';
+}
+elseif ($domain === 'localhost' || $domain === '127.0.0.1' || $domain === '127.0.0.1:8000') {
+    $environmentFile = '.env.local';
+}
+
+$envFilePath = $app->basePath($environmentFile);
+
+if (file_exists($envFilePath)) {
+    $dotenv = Dotenv\Dotenv::createMutable($app->basePath(), $environmentFile);
+    $dotenv->load();
+}
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
