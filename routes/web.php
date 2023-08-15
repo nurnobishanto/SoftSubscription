90a126e8ab9062cbc5311fc1829adf91e666fb8d
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-08-16 00:07:16 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('check-subscription',[\App\Http\Controllers\SubscriptionController::class,'check_subscription']);
+Route::post('check-subscription',[\App\Http\Controllers\SubscriptionController::class,'check_subscription']);
 Route::get('expired/{pid}',[\App\Http\Controllers\SubscriptionController::class,'expired'])->name('expired');
 Route::get('renew/{pid}',[\App\Http\Controllers\SubscriptionController::class,'renew'])->name('renew');
 
@@ -29,6 +30,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('optimize',function (){
+   Artisan::call('optimize');
+   return Artisan::output();
+});
+Route::get('migrate',function (){
+    Artisan::call('migrate');
+    return Artisan::output();
+});
+Route::get('fresh-migrate',function (){
+    Artisan::call('migrate:fresh --seed');
+    return Artisan::output();
 });
 
 require __DIR__.'/auth.php';
